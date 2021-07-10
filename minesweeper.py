eea0da90d00439570f37f7c4ab70c8d3b72c3a84
KittydaCat <46250181+KittydaCat@users.noreply.github.com>
2021-07-10 15:14:49 -0700

    Add files via upload

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,7 @@
 import random
 import tkinter as tk
 from functools import partial
+import time
 
 
 class Sweep:
@@ -28,8 +29,59 @@ class Sweep:
         # the tk display
         self.display = tk.Tk()
 
+    # returns the amount of unmined neighbors
+    def unmined_neighbors(self, cords):
+
+        lis = self.value_neighbors(cords)
+
+        return lis.count('?') + lis.count('F')
+
+    # returns the value of the neighbors for a position
+    def value_neighbors(self, cords):
+
+        xpos, ypos = cords
+        lis = []
+
+        for x in range(-1, 2):
+
+            # if its in range
+            if -1 < xpos + x < self.width:
+
+                for y in range(-1, 2):
+
+                    if -1 < ypos + y < self.height:
+
+                        if abs(x) + abs(y):
+
+                            lis.append(self.view[ypos+y][xpos+x])
+
+        return lis
+
+    # returns the cords of the neighbors for a position
+    def cords_neighbors(self, cords):
+
+        xpos, ypos = cords
+        lis = []
+
+        for x in range(-1, 2):
+
+            # if its in range
+            if -1 < xpos + x < self.width:
+
+                for y in range(-1, 2):
+
+                    if -1 < ypos + y < self.height:
+
+                        if abs(x) + abs(y):
+
+                            lis.append((ypos+y, xpos+x))
+
+        return lis
+
     # if the player mines a square
-    def mine(self, xpos, ypos, recurved=False):
+    def mine(self, cords, recurved=False):
+
+        xpos, ypos = cords
 
         # check if there are any mines and if there isn't
         if True not in ['M' in row for row in self.mines]:
@@ -87,7 +139,9 @@ class Sweep:
         self.update_display()
 
     # flag a mine
-    def flag(self, xpos, ypos, event):
+    def flag(self, cords, event):
+
+        xpos, ypos = cords
 
         if self.view[ypos][xpos] != 'F':
         
@@ -130,9 +184,11 @@ class Sweep:
 
             for c in range(self.width):
 
-                button = tk.Button(self.display, text=self.view[r][c], command=partial(self.mine, c, r))
+                button = tk.Button(self.display, text=self.view[r][c], command=partial(self.mine, (c, r)))
 
-                button.bind('<Button-3>', partial(self.flag, c, r))
+                button.bind('<Button-3>', partial(self.flag, (c, r)))
+
+                button.bind('<Button-2>', partial(print, self.auto_flag((c, r))))
 
                 button.grid(row=r, column=c)
 
@@ -146,8 +202,83 @@ class Sweep:
 
             print('')
 
+    # solve the game of mine sweeper
+    def solve(self):
+
+        working = True
+
+        # while its still making progress
+        while working:
+
+            working = False
+
+            # loop thru all the functions
+            funcs = {self.auto_flag: 'flag', self.auto_mine: 'mine'}
+            for func in funcs:
+
+                for y in range(self.height):
+
+                    for x in range(self.width):
+
+                        print('new')
+
+                        if func((x, y)):
+
+                            working = True
+
+                            if funcs[func] == 'mine':
+
+                                for cords in func((x, y)):
+
+                                    print('mining', cords)
+
+                                    self.mine(cords)
+
+                            else:
+
+                                for cords in func((x, y)):
+
+                                    print('flagging', cords)
+
+                                    self.flag(cords, None)
+
+                            input()
+
+    # return true if the game is solved. else return false.
+    def solved(self):
+
+        if sum([lis.count('?') + lis.count('F') for lis in self.view]) == self.nummines:
+
+            return True
+
+        else:
+
+            return False
+
+    # detects if the value of the square is equal the the amount of unmined neighbors
+    def auto_flag(self, cords):
+
+        xpos, ypos = cords
+
+        # if the number of the square == the amount of neighbors
+        if self.view[ypos][xpos] == self.unmined_neighbors(cords):
+
+            # return the unflaged neighbors
+            return [neighbor for neighbor in self.cords_neighbors(cords) if self.view[neighbor[1]][neighbor[0]] == '?']
+
+    # detects if the value of the square is equal to the amount of mines
+    def auto_mine(self, cords):
+
+        xpos, ypos = cords
+
+        # if the number of the squares == the amount of flagged neighbors
+        if self.view[ypos][xpos] == self.value_neighbors(cords).count('F'):
+
+            # mine all the squares
+            return self.cords_neighbors(cords)
+
 
 if __name__ == '__main__':
 
-    s = Sweep(5, 5, 5)
+    s = Sweep(5, 5, 1)
     s.update_display()
